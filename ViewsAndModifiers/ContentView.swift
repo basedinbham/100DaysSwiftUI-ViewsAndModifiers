bd544f3b1791f245730f2cc8c15645dbbbced3f8
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2022-06-19 18:19:10 -0700

    Added view modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct ProminentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func prominentTitle() -> some View {
+        modifier(ProminentTitle())
+    }
+}
+
 struct Watermark: ViewModifier {
     var text: String
     
